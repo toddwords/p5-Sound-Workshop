e511e2efc877f64ab7c7714d3ce7cb0fe0f729e6
Todd Anderson <toddwords@gmail.com>
2017-10-18 19:35:28 -0400

    add examples

---
+++ micIn/sketch.js
@@ -0,0 +1,25 @@
+var mic;
+
+function setup() {
+  createCanvas(710, 200);
+
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
+}
+
+function draw() {
+  background(200);
+
+  // Get the overall volume (between 0 and 1.0)
+  var vol = mic.getLevel();
+  fill(127);
+  stroke(0);
+
+  // Draw an ellipse with height based on volume
+  var h = map(vol, 0, 1, height, 0);
+  ellipse(width/2, h - 25, 50, 50);
+}
---
+++ soundVis/sketch.js
@@ -0,0 +1,28 @@
+var song, analyzer;
+
+function preload() {
+  song = loadSound('../assets/be.mp3');
+}
+
+function setup() {
+  createCanvas(710, 200);
+  song.loop();
+
+  // create a new Amplitude analyzer
+  analyzer = new p5.Amplitude();
+
+  // Patch the input to an volume analyzer
+  analyzer.setInput(song);
+}
+
+function draw() {
+  background(255);
+
+  // Get the average (root mean square) amplitude
+  var rms = analyzer.getLevel();
+  fill(127);
+  stroke(0);
+
+  // Draw an ellipse with size based on volume
+  ellipse(width/2, height/2, 10+rms*200, 10+rms*200);
+}
